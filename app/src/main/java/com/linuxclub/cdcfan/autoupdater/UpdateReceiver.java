b5eace286694a70c5a188ed9e812bd6302d63d42
Peace Da <daqi1990@gmail.com>
2015-06-12 11:18:11 +0800

    fix potential memory leak of activity

--- app/src/main/java/com/linuxclub/cdcfan/autoupdater/UpdateReceiver.java
+++ app/src/main/java/com/linuxclub/cdcfan/autoupdater/UpdateReceiver.java
@@ -26,26 +26,36 @@ public class UpdateReceiver extends ResultReceiver {
         ((MyApplication) ctx.getApplicationContext()).inject(this);
     }
 
+    public UpdateListener getListener() {
+        return mUpdateListener;
+    }
+
+    public void setListener(UpdateListener listener) {
+        mUpdateListener = listener;
+    }
+
     @Override
     protected void onReceiveResult(int resultCode, Bundle resultData) {
-        if (resultCode == ServiceConst.DOWNLOAD_BEGIN) {
-            long size = resultData.getLong(ServiceConst.KEY_DOWNLOAD_FILE_APK_SIZE, 0);
-            mUpdateListener.onDownloadBegin(size);
-        } else if (resultCode == ServiceConst.DOWNLOAD_RUNNING) {
-            int percent = resultData.getInt(ServiceConst.KEY_DOWNLOAD_PERCENT, -1);
-            if (percent == -1) {
-                mUpdateListener.onDownloadError(new Exception("illegal percent data"));
+        if (mUpdateListener != null) {
+            if (resultCode == ServiceConst.DOWNLOAD_BEGIN) {
+                long size = resultData.getLong(ServiceConst.KEY_DOWNLOAD_FILE_APK_SIZE, 0);
+                mUpdateListener.onDownloadBegin(size);
+            } else if (resultCode == ServiceConst.DOWNLOAD_RUNNING) {
+                int percent = resultData.getInt(ServiceConst.KEY_DOWNLOAD_PERCENT, -1);
+                if (percent == -1) {
+                    mUpdateListener.onDownloadError(new Exception("illegal percent data"));
+                } else {
+                    mUpdateListener.onDownloading(percent);
+                }
+            } else if (resultCode == ServiceConst.DOWNLOAD_SUCC) {
+                mUpdateListener.onDownloadSucc();
+                mUpdateMgr.install();
+            } else if (resultCode == ServiceConst.DOWNLOAD_CANCELED) {
+                mUpdateListener.onDownloadCanceled();
             } else {
-                mUpdateListener.onDownloading(percent);
+                Exception e = (Exception) resultData.getSerializable(ServiceConst.KEY_DOWNLOAD_EXCEPTION);
+                mUpdateListener.onDownloadError(e);
             }
-        } else if (resultCode == ServiceConst.DOWNLOAD_SUCC) {
-            mUpdateListener.onDownloadSucc();
-            mUpdateMgr.install();
-        } else if (resultCode == ServiceConst.DOWNLOAD_CANCELED) {
-            mUpdateListener.onDownloadCanceled();
-        } else {
-            Exception e = (Exception) resultData.getSerializable(ServiceConst.KEY_DOWNLOAD_EXCEPTION);
-            mUpdateListener.onDownloadError(e);
         }
     }
 
